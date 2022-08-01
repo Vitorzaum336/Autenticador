d0339e0841c905322a87661b3bc516821c34f5c5
yazmiiim <yasmimsantos63@outlook.com>
2022-08-01 08:55:45 -0300

    v1.0

---
+++ app/src/main/java/com/example/autenticador/MainActivity.kt
@@ -0,0 +1,19 @@
+package com.example.autenticador
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
+import com.google.firebase.provider.FirebaseInitProvider
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var auth: FirebaseAuth
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        auth = Firebase.auth
+    }
+}
